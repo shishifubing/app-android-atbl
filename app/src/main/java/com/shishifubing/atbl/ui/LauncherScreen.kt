03f5e691fa5a17ac681dc8d5880ef0ac84296e2a
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-10-21 17:49:45 +0300

    feat(launcher): improve drawer dialog

--- app/src/main/java/com/shishifubing/atbl/ui/LauncherScreen.kt
+++ app/src/main/java/com/shishifubing/atbl/ui/LauncherScreen.kt
@@ -1,6 +1,7 @@
 package com.shishifubing.atbl.ui
 
 import android.graphics.Bitmap
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
@@ -8,14 +9,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -94,7 +102,7 @@ fun LauncherScreen(
             app = app,
             vm = vm,
             onDismissRequest = { dialogApp = null },
-            showHideAppButton = app.packageName != LocalContext.current.packageName
+            enabledHide = app.packageName != LocalContext.current.packageName
         )
     }
 }
@@ -166,54 +174,69 @@ fun AppDialog(
     app: LauncherApp,
     vm: LauncherViewModel,
     onDismissRequest: () -> Unit,
-    showHideAppButton: Boolean,
+    enabledHide: Boolean,
     modifier: Modifier = Modifier
 ) {
     Dialog(onDismissRequest = onDismissRequest) {
         ElevatedCard(modifier = modifier) {
-            Column {
-                Column(
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+                Image(
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .size(dimensionResource(R.dimen.image_size)),
+                    bitmap = vm.getAppIcon(app.packageName)
+                        .toBitmap(config = Bitmap.Config.ARGB_8888)
+                        .asImageBitmap(),
+                    contentDescription = "App icon",
+                )
+                Text(
+                    text = app.label,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small)),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(dimensionResource(R.dimen.padding_small)),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                        .padding(
+                            dimensionResource(R.dimen.padding_small),
+                            dimensionResource(R.dimen.padding_medium)
+                        )
                 ) {
-                    Image(
-                        modifier = Modifier
-                            .padding(dimensionResource(R.dimen.padding_small))
-                            .size(dimensionResource(R.dimen.image_size)),
-                        bitmap = vm.getAppIcon(app.packageName)
-                            .toBitmap(config = Bitmap.Config.ARGB_8888)
-                            .asImageBitmap(),
-                        contentDescription = "App icon",
+                    AppDialogButton(
+                        modifier = Modifier.weight(1f),
+                        text = R.string.drawer_app_info,
+                        shapeType = 0,
+                        onClick = {
+                            vm.launchAppInfo(app.packageName)
+                            onDismissRequest()
+                        }
                     )
-                    Text(
-                        text = app.label,
-                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small)),
-                        style = MaterialTheme.typography.headlineSmall
-                    )
-                }
-                Divider()
-                AppDialogItem(
-                    text = stringResource(R.string.drawer_app_info),
-                    onClick = {
-                        vm.launchAppInfo(app.packageName)
-                        onDismissRequest()
-                    }
-                )
-                AppDialogItem(
-                    text = stringResource(R.string.drawer_app_uninstall),
-                    onClick = {
-                        vm.launchAppUninstall(app.packageName)
-                        onDismissRequest()
-                    }
-                )
-                if (showHideAppButton) {
-                    AppDialogItem(
-                        text = stringResource(R.string.drawer_app_hide),
+                    Spacer(modifier = Modifier.width(2.dp))
+                    AppDialogButton(
+                        modifier = Modifier.weight(1f),
+                        text = R.string.drawer_app_hide,
+                        enabled = enabledHide,
+                        shapeType = 1,
                         onClick = {
                             vm.hideApp(app.packageName)
+                            onDismissRequest()
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(2.dp))
+                    AppDialogButton(
+                        modifier = Modifier.weight(1f),
+                        text = R.string.drawer_app_uninstall,
+                        shapeType = 2,
+                        onClick = {
+                            vm.launchAppUninstall(app.packageName)
                             onDismissRequest()
                         }
                     )
@@ -229,9 +252,18 @@ fun AppDialog(
                 ) {
                     items(app.shortcutsList.size) { i ->
                         val shortcut = app.shortcutsList[i]
-                        AppDialogItem(text = shortcut.label) {
-                            vm.launchAppShortcut(shortcut)
-                            onDismissRequest()
+                        Surface(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                vm.launchAppShortcut(shortcut)
+                                onDismissRequest()
+                            },
+                        ) {
+                            Text(
+                                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+                                text = shortcut.label,
+                                style = MaterialTheme.typography.bodyLarge
+                            )
                         }
                     }
 
@@ -242,20 +274,39 @@ fun AppDialog(
 }
 
 @Composable
-fun AppDialogItem(
-    text: String,
+fun AppDialogButton(
+    @StringRes text: Int,
+    shapeType: Int,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    enabled: Boolean = true
 ) {
-    Surface(
-        modifier = modifier
-            .fillMaxWidth(),
-        onClick = onClick
+    val shape = when (shapeType) {
+        0 -> RoundedCornerShape(
+            dimensionResource(R.dimen.padding_medium),
+            0.dp,
+            0.dp,
+            dimensionResource(R.dimen.padding_medium),
+        )
+
+        2 -> RoundedCornerShape(
+            0.dp,
+            dimensionResource(R.dimen.padding_medium),
+            dimensionResource(R.dimen.padding_medium),
+            0.dp,
+        )
+
+        else -> RectangleShape
+    }
+    ElevatedButton(
+        modifier = modifier,
+        onClick = onClick,
+        shape = shape,
+        enabled = enabled
     ) {
         Text(
-            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
-            text = text,
-            style = MaterialTheme.typography.bodyLarge
+            text = stringResource(text),
+            style = MaterialTheme.typography.bodySmall
         )
     }
 }
