68e2ff8ca8621ad04b1161ce867bda7a4594d43d
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-11-20 17:34:03 +0300

    refactor(ui): more refactoring

--- app/src/main/java/com/shishifubing/atbl/ui/LauncherTheme.kt
+++ app/src/main/java/com/shishifubing/atbl/ui/LauncherTheme.kt
@@ -2,12 +2,15 @@ package com.shishifubing.atbl.ui
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 
 private val colorSchemes = object {
@@ -33,5 +36,7 @@ fun LauncherTheme(
         else -> colorSchemes.light
     }
 
-    MaterialTheme(colorScheme = colorScheme, content = content)
+    MaterialTheme(colorScheme = colorScheme) {
+        Surface(modifier = Modifier.fillMaxSize(), content = content)
+    }
 }
--- app/src/main/java/com/shishifubing/atbl/ui/SettingsViewModel.kt
+++ app/src/main/java/com/shishifubing/atbl/ui/SettingsViewModel.kt
@@ -12,39 +12,35 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.shishifubing.atbl.LauncherApp
 import com.shishifubing.atbl.LauncherApplication
-import com.shishifubing.atbl.LauncherHorizontalArrangement
 import com.shishifubing.atbl.LauncherSettings
 import com.shishifubing.atbl.LauncherSplitScreenShortcut
-import com.shishifubing.atbl.LauncherTextColor
 import com.shishifubing.atbl.domain.LauncherAppsRepository
 import com.shishifubing.atbl.domain.LauncherSettingsRepository
-import com.shishifubing.atbl.domain.LauncherSettingsSerializer
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class SettingsField<T>(
-    private val flow: Flow<T>,
-    val setter: (T) -> Unit
-) {
-    @Composable
-    fun collectAsState(): State<T> {
-        return flow.collectAsState(runBlocking { flow.first() })
-    }
-}
 
 class SettingsViewModel(
     private val settingsRepo: LauncherSettingsRepository,
-    private val appsRepo: LauncherAppsRepository,
-    val initialSettings: LauncherSettings
+    private val appsRepo: LauncherAppsRepository
 ) : ViewModel() {
 
-    val settingsFlow = settingsRepo.settingsFlow
+    private val settingsFlow = settingsRepo.settingsFlow
     val shortcutsFlow = appsRepo.appsFlow.map { it.splitScreenShortcutsList }
     val appsFlow = appsRepo.appsFlow.map { it.appsMap.values }
+    private val _error = MutableStateFlow<Throwable?>(null)
+    val error = _error.asStateFlow()
+    private val exceptionHandler = CoroutineExceptionHandler { _, e ->
+        _error.update { e }
+    }
 
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
@@ -52,17 +48,29 @@ class SettingsViewModel(
                 val app = this[APPLICATION_KEY] as LauncherApplication
                 SettingsViewModel(
                     settingsRepo = app.settingsRepo!!,
-                    appsRepo = app.appsRepo!!,
-                    initialSettings = runBlocking { app.settingsRepo!!.fetchInitial() }
+                    appsRepo = app.appsRepo!!
                 )
             }
         }
     }
 
-    private fun launch(action: suspend CoroutineScope.() -> Unit) {
-        viewModelScope.launch { action() }
+    inner class SettingsField<T>(
+        private val flow: Flow<T>,
+        private val setter: suspend CoroutineScope.(
+            LauncherSettings.Builder
+        ) -> (T) -> LauncherSettings.Builder
+    ) {
+        fun set(newValue: T) = updateSettings { setter(it)(newValue) }
+
+        @Composable
+        fun collectAsState(): State<T> {
+            return flow.collectAsState(runBlocking { flow.first() })
+        }
     }
 
+    private fun launch(action: suspend CoroutineScope.() -> Unit) {
+        viewModelScope.launch(exceptionHandler) { action() }
+    }
 
     fun updateSettings(
         action: suspend CoroutineScope.(LauncherSettings.Builder) -> (LauncherSettings.Builder)
@@ -74,48 +82,79 @@ class SettingsViewModel(
         launch { settingsRepo.updateFromBytes(bytes) }
     }
 
-    val settings = SettingsField(flow = settingsFlow) {
-        launch { settingsRepo.update { it } }
-    }
+    val settings = SettingsField(
+        flow = settingsFlow, setter = { { newValue -> newValue.toBuilder() } }
+    )
 
     val splitScreenSeparator = SettingsField(
-        flow = settingsFlow.map { it.appCardSplitScreenSeparator }
-    ) { newValue ->
-        updateSettings { it.setAppCardSplitScreenSeparator(newValue) }
-    }
+        flow = settingsFlow.map { it.appCardSplitScreenSeparator },
+        setter = { it::setAppCardSplitScreenSeparator }
+    )
+
+    val appLayoutReverseOrder = SettingsField(
+        flow = settingsFlow.map { it.appLayoutReverseOrder },
+        setter = { it::setAppLayoutReverseOrder }
+    )
+
+    val appLayoutVerticalArrangement = SettingsField(
+        flow = settingsFlow.map { it.appLayoutVerticalArrangement },
+        setter = { it::setAppLayoutVerticalArrangement }
+    )
+
+    val appLayoutHorizontalArrangement = SettingsField(
+        flow = settingsFlow.map { it.appLayoutHorizontalArrangement },
+        setter = { it::setAppLayoutHorizontalArrangement }
+    )
+
+    val appLayoutHorizontalPadding = SettingsField(
+        flow = settingsFlow.map { it.appLayoutHorizontalPadding },
+        setter = { it::setAppLayoutHorizontalPadding }
+    )
+
+    val appLayoutVerticalPadding = SettingsField(
+        flow = settingsFlow.map { it.appLayoutVerticalPadding },
+        setter = { it::setAppLayoutVerticalPadding }
+    )
+
+    val appLayoutSortBy = SettingsField(
+        flow = settingsFlow.map { it.appLayoutSortBy },
+        setter = { it::setAppLayoutSortBy }
+    )
 
     val appCardRemoveSpaces = SettingsField(
-        flow = settingsFlow.map { it.appCardLabelRemoveSpaces }
-    ) { newValue ->
-        updateSettings { it.setAppCardLabelRemoveSpaces(newValue) }
-    }
+        flow = settingsFlow.map { it.appCardLabelRemoveSpaces },
+        setter = { it::setAppCardLabelRemoveSpaces }
+    )
 
-    fun backupReset() {
-        updateSettings { LauncherSettingsSerializer.defaultValue.toBuilder() }
-    }
+    val appCardLabelLowercase = SettingsField(
+        flow = settingsFlow.map { it.appCardLabelLowercase },
+        setter = { it::setAppCardLabelLowercase }
+    )
 
-    fun setHorizontalArrangement(
-        horizontalArrangement: LauncherHorizontalArrangement
-    ) {
-        updateSettings {
-            it.setAppLayoutHorizontalArrangement(horizontalArrangement)
-        }
-    }
+    val appCardTextColor = SettingsField(
+        flow = settingsFlow.map { it.appCardTextColor },
+        setter = { it::setAppCardTextColor }
+    )
 
-    fun setAppCardLowercase(lowercase: Boolean) {
-        updateSettings { it.setAppCardLabelLowercase(lowercase) }
-    }
+    val appCardPadding = SettingsField(
+        flow = settingsFlow.map { it.appCardPadding },
+        setter = { it::setAppCardPadding }
+    )
 
-    fun setAppCardTextColor(textColor: LauncherTextColor) {
-        updateSettings { it.setAppCardTextColor(textColor) }
-    }
+    val appCardTextStyle = SettingsField(
+        flow = settingsFlow.map { it.appCardTextStyle },
+        setter = { it::setAppCardTextStyle }
+    )
 
-    fun setAppCardPadding(padding: Int) {
-        updateSettings { it.setAppCardPadding(padding) }
-    }
+    val appCardFontFamily = SettingsField(
+        flow = settingsFlow.map { it.appCardFontFamily },
+        setter = { it::setAppCardFontFamily }
+    )
 
-    fun setHiddenApps(hiddenPackages: List<String>) {
-        launch { appsRepo.setHiddenApps(hiddenPackages) }
+    fun backupReset() = updateSettings { settingsRepo.getDefault() }
+
+    fun setHiddenApps(packageNames: List<String>) {
+        launch { appsRepo.setHiddenApps(packageNames) }
     }
 
     fun addSplitScreenShortcut(appTop: LauncherApp, appBottom: LauncherApp) {
