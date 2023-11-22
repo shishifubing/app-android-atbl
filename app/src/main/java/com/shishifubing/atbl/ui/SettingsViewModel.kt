9a98416596657573b938dacf1b5872d68b92ea4c
q1feq3qzi60u <97828377+q1feq3qzi60u@users.noreply.github.com>
2023-11-22 20:14:57 +0300

    refactor(ui): add uistate to settings

--- app/src/main/java/com/shishifubing/atbl/ui/SettingsViewModel.kt
+++ app/src/main/java/com/shishifubing/atbl/ui/SettingsViewModel.kt
@@ -1,9 +1,6 @@
 package com.shishifubing.atbl.ui
 
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -18,11 +15,11 @@ import com.shishifubing.atbl.domain.LauncherAppsRepository
 import com.shishifubing.atbl.domain.LauncherSettingsRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -32,16 +29,6 @@ class SettingsViewModel(
     private val settingsRepo: LauncherSettingsRepository,
     private val appsRepo: LauncherAppsRepository
 ) : ViewModel() {
-
-    private val settingsFlow = settingsRepo.settingsFlow
-    val shortcutsFlow = appsRepo.appsFlow.map { it.splitScreenShortcutsList }
-    val appsFlow = appsRepo.appsFlow.map { it.appsMap.values }
-    private val _error = MutableStateFlow<Throwable?>(null)
-    val error = _error.asStateFlow()
-    private val exceptionHandler = CoroutineExceptionHandler { _, e ->
-        _error.update { e }
-    }
-
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
@@ -54,121 +41,91 @@ class SettingsViewModel(
         }
     }
 
-    inner class SettingsField<T>(
-        private val flow: Flow<T>,
-        private val setter: suspend CoroutineScope.(
-            LauncherSettings.Builder
-        ) -> (T) -> LauncherSettings.Builder
-    ) {
-        fun set(newValue: T) = updateSettings { setter(it)(newValue) }
+    private val _error = MutableStateFlow<Throwable?>(null)
+    val error = _error.asStateFlow()
 
-        @Composable
-        fun collectAsState(): State<T> {
-            return flow.collectAsState(runBlocking { flow.first() })
+    private val exceptionHandler = CoroutineExceptionHandler { _, e ->
+        _error.update { e }
+    }
+
+    val uiState = combine(
+        appsRepo.appsFlow,
+        settingsRepo.settingsFlow
+    ) { apps, settings ->
+        SettingsScreenUiState.Success(
+            apps = apps.appsMap.values,
+            splitScreenShortcuts = apps.splitScreenShortcutsList,
+            settings = settings
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = SettingsScreenUiState.Loading
+    )
+
+    val settingsActions = object : SettingsActions {
+        override fun updateSettings(
+            action: suspend LauncherSettings.Builder.() -> LauncherSettings.Builder
+        ) {
+            launch { settingsRepo.update { runBlocking { action(it) } } }
         }
+
+        override fun updateSettingsFromBytes(bytes: ByteArray) {
+            launch { settingsRepo.updateFromBytes(bytes) }
+        }
+
+        override fun backupReset() {
+            updateSettings { settingsRepo.getDefault() }
+        }
+
+        override fun setHiddenApps(packageNames: List<String>) {
+            launch { appsRepo.setHiddenApps(packageNames) }
+        }
+
+        override fun addSplitScreenShortcut(
+            appTop: LauncherApp,
+            appBottom: LauncherApp
+        ) {
+            launch {
+                appsRepo.addSplitScreenShortcut(
+                    appTop.packageName,
+                    appBottom.packageName
+                )
+            }
+        }
+
+        override fun removeSplitScreenShortcut(shortcut: LauncherSplitScreenShortcut) {
+            launch {
+                appsRepo.removeSplitScreenShortcut(shortcut)
+            }
+        }
+
     }
 
     private fun launch(action: suspend CoroutineScope.() -> Unit) {
         viewModelScope.launch(exceptionHandler) { action() }
     }
+}
 
-    fun updateSettings(
-        action: suspend CoroutineScope.(LauncherSettings.Builder) -> (LauncherSettings.Builder)
-    ) {
-        launch { settingsRepo.update { runBlocking { action(it) } } }
-    }
+sealed interface SettingsScreenUiState {
+    data class Success(
+        val settings: LauncherSettings,
+        val apps: Collection<LauncherApp>,
+        val splitScreenShortcuts: List<LauncherSplitScreenShortcut>
+    ) : SettingsScreenUiState
 
-    fun updateSettingsFromBytes(bytes: ByteArray) {
-        launch { settingsRepo.updateFromBytes(bytes) }
-    }
+    object Loading : SettingsScreenUiState
+}
 
-    val settings = SettingsField(
-        flow = settingsFlow, setter = { { newValue -> newValue.toBuilder() } }
-    )
+interface SettingsActions {
+    fun updateSettings(action: suspend LauncherSettings.Builder.() -> LauncherSettings.Builder)
 
-    val splitScreenSeparator = SettingsField(
-        flow = settingsFlow.map { it.appCardSplitScreenSeparator },
-        setter = { it::setAppCardSplitScreenSeparator }
-    )
+    fun updateSettingsFromBytes(bytes: ByteArray)
 
-    val appLayoutReverseOrder = SettingsField(
-        flow = settingsFlow.map { it.appLayoutReverseOrder },
-        setter = { it::setAppLayoutReverseOrder }
-    )
+    fun backupReset()
 
-    val appLayoutVerticalArrangement = SettingsField(
-        flow = settingsFlow.map { it.appLayoutVerticalArrangement },
-        setter = { it::setAppLayoutVerticalArrangement }
-    )
+    fun setHiddenApps(packageNames: List<String>)
 
-    val appLayoutHorizontalArrangement = SettingsField(
-        flow = settingsFlow.map { it.appLayoutHorizontalArrangement },
-        setter = { it::setAppLayoutHorizontalArrangement }
-    )
-
-    val appLayoutHorizontalPadding = SettingsField(
-        flow = settingsFlow.map { it.appLayoutHorizontalPadding },
-        setter = { it::setAppLayoutHorizontalPadding }
-    )
-
-    val appLayoutVerticalPadding = SettingsField(
-        flow = settingsFlow.map { it.appLayoutVerticalPadding },
-        setter = { it::setAppLayoutVerticalPadding }
-    )
-
-    val appLayoutSortBy = SettingsField(
-        flow = settingsFlow.map { it.appLayoutSortBy },
-        setter = { it::setAppLayoutSortBy }
-    )
-
-    val appCardRemoveSpaces = SettingsField(
-        flow = settingsFlow.map { it.appCardLabelRemoveSpaces },
-        setter = { it::setAppCardLabelRemoveSpaces }
-    )
-
-    val appCardLabelLowercase = SettingsField(
-        flow = settingsFlow.map { it.appCardLabelLowercase },
-        setter = { it::setAppCardLabelLowercase }
-    )
-
-    val appCardTextColor = SettingsField(
-        flow = settingsFlow.map { it.appCardTextColor },
-        setter = { it::setAppCardTextColor }
-    )
-
-    val appCardPadding = SettingsField(
-        flow = settingsFlow.map { it.appCardPadding },
-        setter = { it::setAppCardPadding }
-    )
-
-    val appCardTextStyle = SettingsField(
-        flow = settingsFlow.map { it.appCardTextStyle },
-        setter = { it::setAppCardTextStyle }
-    )
-
-    val appCardFontFamily = SettingsField(
-        flow = settingsFlow.map { it.appCardFontFamily },
-        setter = { it::setAppCardFontFamily }
-    )
-
-    fun backupReset() = updateSettings { settingsRepo.getDefault() }
-
-    fun setHiddenApps(packageNames: List<String>) {
-        launch { appsRepo.setHiddenApps(packageNames) }
-    }
-
-    fun addSplitScreenShortcut(appTop: LauncherApp, appBottom: LauncherApp) {
-        launch {
-            appsRepo.addSplitScreenShortcut(
-                appTop.packageName,
-                appBottom.packageName
-            )
-        }
-    }
-
-    fun removeSplitScreenShortcut(shortcut: LauncherSplitScreenShortcut) {
-        launch {
-            appsRepo.removeSplitScreenShortcut(shortcut)
-        }
-    }
+    fun addSplitScreenShortcut(appTop: LauncherApp, appBottom: LauncherApp)
+    fun removeSplitScreenShortcut(shortcut: LauncherSplitScreenShortcut)
 }
